bde9c315648356d983abf3dbb921cf5fe77131d4
Okoye Divine <66203222+okoye-dev@users.noreply.github.com>
2024-06-19 10:04:34 +0100

    Update HeroImage.jsx

--- src/components/hero/HeroImage.jsx
+++ src/components/hero/HeroImage.jsx
@@ -5,7 +5,6 @@ const HeroImage = () => {
   return (
     <section className="absolute left-1/2 top-0 z-[90] hidden max-h-[500px] w-1/2 max-w-[400px] overflow-hidden rounded-br-3xl bg-slate-600 sm:flex lg:max-h-[600px] lg:max-w-[600px]">
       <img
-        loading="lazy"
         src={heroImg}
         alt="heroImg"
         width={"500"}
